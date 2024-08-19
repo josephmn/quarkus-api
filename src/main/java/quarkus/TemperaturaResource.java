f27ad2e31086e6efa50067c8bd6b51c2662b01d0
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-08-19 12:10:35 -0500

    definiendo un endpoint POST

--- src/main/java/quarkus/TemperaturaResource.java
+++ src/main/java/quarkus/TemperaturaResource.java
@@ -1,25 +1,38 @@
 package quarkus;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Path("/temperaturas")
 public class TemperaturaResource {
 
+    private List<Temperatura> valores = new ArrayList<>();
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Temperatura nueva(Temperatura temp) {
+        valores.add(temp);
+        return temp;
+    }
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Temperatura> list() {
-        return Arrays.asList(
+        /*return Arrays.asList(
                 new Temperatura("Madrid", 18, 28),
                 new Temperatura("Málaga", 15, 31),
                 new Temperatura("Tenerife", 20, 30),
                 new Temperatura("Bogotá", 13, 27)
-        );
+        );*/
+        return Collections.unmodifiableList(valores);
     }
 
     @GET
